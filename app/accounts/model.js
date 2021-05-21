dbaac549062940423d4cc03dbb1b934fb397e808
Efen Stakes <efenstakes101@gmail.com>
2021-05-21 11:51:19 +0300

    add accounts model

---
+++ app/accounts/model.js
@@ -0,0 +1,66 @@
+// include external libraries
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+
+const { generate_code } = require('../utils/utils')
+
+
+const AccountSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: (mail) => {
+            return validator.isEmail(mail)
+        }
+    },
+
+    phone: {
+        type: String,
+        required: false
+    },
+
+    joined_on: {
+        type: Date,
+        default: Date.now()
+    },
+
+    verified: {
+        code: { type: String },
+        issued_at: { 
+            type: Date, 
+            default: Date.now(),
+        },
+        done: { type: Boolean, default: false }
+    }
+
+})
+
+// hash the password before saving a record
+UserSchema.pre('save', function(next) {
+    this.password = bcrypt.hashSync(this.password, 10)
+    
+    let code = generate_code()
+
+    this.verified = { code, done: false }    
+    next()
+})
+
+// delete any account data which may need deleting
+AccountSchema.pre('remove', function() {})
+
+const Account = mongoose.model('Account', AccountSchema)
+
+module.exports = Account
